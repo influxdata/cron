75c8de5674aec41e9a7ba908d4d224a40493c58e
docmerlin <landivar@gmail.com>
2019-06-29 04:09:26 -0500

    WIP

--- cron.go
+++ cron.go
@@ -7,6 +7,35 @@ import (
 	"time"
 )
 
+type years struct {
+	low  uint64
+	high uint64
+}
+
+func (y years) TrailingZeros64() {
+	zeros := bits.TrailingZeros64(y.low)
+	if zeros == 64 {
+		zeros += bits.TrailingZeros64(y.high)
+	}
+	return zeros
+}
+
+func (y years) Intersection(y2 years) years {
+	return years{
+		low:  y.low & y2.low,
+		high: y.high & y2.high,
+	}
+}
+
+// this is undefined if the year is above 2070 or below 1970
+func (ys years)in(year int) bool{
+	y:=uint64(year-1970)
+	if y >64{
+		return ys.high & (1<<(y-64)) >0
+	}
+	return ys.low & (1<<y)>0
+}
+
 type nextTime struct {
 	second uint64
 	minute uint64
@@ -14,6 +43,8 @@ type nextTime struct {
 	dow    uint64
 	month  uint64
 	dom    uint64
+	year   years
+	loc    *time.Location
 }
 
 var maxMonthLengths = [12]int{1 << 31, 1 << 28, 1 << 31, 1 << 30, 1 << 31, 1 << 30, 1 << 31, 1 << 31, 1 << 30, 1 << 31, 1 << 30, 1 << 31}
@@ -53,7 +84,7 @@ func (nt *nextTime) nextMonth(m, d uint64) time.Month {
 	// no need to increment month, because we zero index and the result of Date is 1 indexed
 	m = uint64(bits.TrailingZeros64(nt.month>>m)) + m
 	d = uint64(bits.TrailingZeros64(nt.dom>>d)) + d
-	if nt.month < 1<<m {
+	if nt.month < 1<<m { // if there is no next avaliable months
 		return -1
 	}
 
@@ -64,10 +95,13 @@ func (nt *nextTime) nextMonth(m, d uint64) time.Month {
 	return time.Month(m)
 }
 
-func (nt *nextTime) nextDayOfMonth(m, d uint64) int {
+func (nt *nextTime) nextDay(y, m, d uint64) int {
 	// no need to increment day, because we zero index and the result of Date is 1 indexed
 	// find next avaliable dom
-	d = uint64(bits.TrailingZeros64(nt.dom>>d)) + d
+	const sundaysAtFirst = uint(1|1 < 7|1 < 14|1 < 21|1 < 28)
+	firstOfMonth := time.Date(y, m, 0, 0, 0, 0, 0, nt.Zone)
+	days := ((sundaysAtFirst << uint(firstOfMonth.Weekday())) * nt.dow) & nt.dom
+	d = uint64(bits.TrailingZeros64(days>>d)) + d
 	if d >= uint64(maxMonthLengths[m]) {
 		return -1
 	}
@@ -101,34 +135,28 @@ func (nt *nextTime) nextSecond(s uint64) int {
 	return int(s)
 }
 
-func (nt *nextTime) next(from time.Time) time.Time {
-	y, M, d := from.Date()
+func (nt *nextTime) nextTime(from time.Time) (h,m,s int, advanceDay bool){
 	h := from.Hour()
 	m := from.Minute()
 	s := from.Second()
 
+func (nt *nextTime) next(from time.Time) (time.Time, error) {
+	y, M, d := from.Date()
+	h := from.Hour()
+	m := from.Minute()
+	s := from.Second()
+	//res:=nt.intersection(timebm(from).Add(time.Second))
+	advance_day:=false
+	// handle time of day
 	if s2 := nt.nextSecond(uint64(s)); s2 < 0 {
-		s = bits.TrailingZeros64(nt.second)
+		s = bits.TrailingZeros64(nt.second)// if not found set to first second and advance minute
 		if m2 := nt.nextMinute(uint64(m)); m2 < 0 {
-			m = bits.TrailingZeros64(nt.minute)
+			m = bits.TrailingZeros64(nt.minute) // if not found set to first minute and advance hour
 			if h2 := nt.nextHour(uint64(h)); h2 < 0 {
-				h = bits.TrailingZeros64(nt.hour)
-				if d2 := nt.nextDayOfMonth(uint64(y), uint64(M)); d2 < 0 {
-					d = bits.TrailingZeros64(nt.month)
-					if M2 := nt.nextMonth(uint64(M), uint64(d)); M2 < 0 {
-						M = 0
-						y2 := nt.nextYear(uint64(y), uint64(M), uint64(d))
-						if y2 < 0 {
-							return time.Time{}
-						}
-						y = y2
-					} else {
-						M = M2
-					}
-				} else {
-					d = d2
-				}
-			} else {
+				h = bits.TrailingZeros64(nt.hour) // if not found set to hour and advance day
+				advanceDay = true
+			}
+			else {
 				h = h2
 			}
 		} else {
@@ -137,8 +165,81 @@ func (nt *nextTime) next(from time.Time) time.Time {
 	} else {
 		s = s2
 	}
-	return time.Date(y, M, d, h, m, s, 0, from.Location())
+
+	// handle date
+	// first we have to check the year
+	if !nt.year.in(y){
+		y2 := nt.nextYear(uint64(y), uint64(M), uint64(d))
+		if y2 < 0 {
+			return time{}, errors.New("could not fulfil schedule") // zero time means that it couldn't schedule something.
+		}
+	}
+	if !nt.month&(1<<m)>0{ //check the month
+		M2 := nt.nextMonth(uint64(M), uint64(d))
+		if M2<0 {
+			M = bits.TrailingZeros64(nt.month)
+			y2 := nt.nextYear(uint64(y), uint64(M), uint64(d))
+			if y2 < 0 {
+				return time{}, errors.New("could not fulfil schedule") // zero time means that it couldn't schedule something.
+			}
+		}
+	}
+
+
+	for y <= 2099 /* maximum year handleable by our crons and also other various crons */ {
+		if d2 := nt.nextDay(uint64(y), uint64(M)); d2 < 0 {
+		if M2 := nt.nextMonth(uint64(M), uint64(d)); M2 < 0 {
+			M = bits.TrailingZeros64(nt.month)
+			y2 := nt.nextYear(uint64(y), uint64(M), uint64(d))
+			if y2 < 0 {
+				return time{}, errors.New("could not fulfil schedule") // zero time means that it couldn't schedule something.
+			}
+		} else {
+			M = M2
+		}
+	}
+
+	return time.Date(y,M,d,h,m,s,0,from.Location), nil
 }
+
+
+
+// 	if s2 := nt.nextSecond(uint64(s)); s2 < 0 {
+// 		s = bits.TrailingZeros64(nt.second)// if not found set to first second and advance minute
+// 		if m2 := nt.nextMinute(uint64(m)); m2 < 0 {
+// 			m = bits.TrailingZeros64(nt.minute) // if not found set to first minute and advance hour
+// 			if h2 := nt.nextHour(uint64(h)); h2 < 0 {
+// 				h = bits.TrailingZeros64(nt.hour) // if not found set to hour and advance day
+// 				if d2 := nt.nextDay(uint64(y), uint64(M)); d2 < 0 {
+// 					d = bits.TrailingZeros64(nt.month) // if not found set to first day and advance month
+
+// 					if M2 := nt.nextMonth(uint64(M), uint64(d)); M2 < 0 {
+// 						M = 0
+// 						y2 := nt.nextYear(uint64(y), uint64(M), uint64(d))
+// 						if y2 < 0 {
+// 							return time.Time{}
+// 						}
+// 						y = y2
+// 					} else {
+// 						M = M2
+// 					}
+// 				} else {
+// 					d = d2
+// 				}
+// 			} else {
+// 				h = h2
+// 			}
+// 		} else {
+// 			m = m2
+// 		}
+// 	} else {
+// 		s = s2
+// 	}
+
+
+
+// 	return 
+// }
 
 func (nt *nextTime) intersectionExists(ct *nextTime) bool {
 	return ct.month&nt.month != 0 &&
@@ -146,6 +247,17 @@ func (nt *nextTime) intersectionExists(ct *nextTime) bool {
 		ct.hour&nt.hour != 0 &&
 		ct.minute&nt.minute != 0 &&
 		ct.second&nt.second != 0
+}
+
+func (nt *nextTime)intersection(ct *nextTime) *nextTime
+	return nextTime{
+		second: nt.second&ct.second,
+		minute: nt.minute&ct.minute,
+		hour:   nt.hour&ct.hour,
+		dow:    nt.dow&ct.dow,
+		month:  nt.month&ct.month,
+		dom:    nt.dom&ct.dom,
+	}
 }
 
 func timebm(ts time.Time) nextTime {
